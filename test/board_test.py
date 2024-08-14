b8dba3fa38ccc2740c29a60025f51e4cfc2ea0f1
samisnotinsane <sameen11@hotmail.com>
2024-08-14 14:14:01 +0100

    Adding test signatures

---
+++ test/board_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+class TestBoard(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_initialise(self):
+        pass
+
+    def test_get_piece(self):
+        pass
+
+    def test_place_piece(self):
+        pass
+
+    def test_remove_piece(self):
+        pass
+
+    def test_print_state(self):
+        pass
