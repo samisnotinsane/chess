8792f87ba06c712462b76637309a325a70bcb9f1
samisnotinsane <sameen11@hotmail.com>
2024-08-16 22:22:23 +0100

    Adding move representation

---
+++ chess/move.py
@@ -0,0 +1,45 @@
+class Move:
+    def __init__(self, src_pos: str, dest_pos: str, piece: str):
+        self._source: str = src_pos
+        self._destination: str = dest_pos
+        self._piece: str = piece
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Move):
+            return (
+                self._source == other._source
+                and self._destination == other._destination
+                and self._piece == other._piece
+            )
+        return False
+
+    def __repr__(self) -> str:
+        return f"Move(src={self._source},dest={self._destination},piece={self._piece})"
+
+    def move_to_str(self) -> str:
+        return f"{self._piece}{self._source}{self._destination}"
+
+    def is_valid_piece(self, piece: str) -> bool:
+        return piece.upper() in ["P", "N", "B", "R", "Q", "K"]
+
+    def is_valid_position(self, pos: str) -> bool:
+        if len(pos) != 2:
+            return False
+        col, row = pos[0], pos[1]
+        if col.lower() not in ["a", "b", "c", "d", "e", "f", "g", "h"]:
+            return False
+        if row not in list(range(1, 9)):
+            return False
+        return True
+
+    @property
+    def source_position(self) -> str:
+        return self._source
+
+    @property
+    def destination_position(self) -> str:
+        return self._destination
+
+    @property
+    def piece(self) -> str:
+        return self._piece
