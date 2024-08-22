43ae1638c877acbb1063eb85844aae2087ddbbed
samisnotinsane <sameen11@hotmail.com>
2024-08-22 22:41:45 +0100

    Refactoring tests

--- test/game/test_move.py
+++ test/game/test_move.py
@@ -1,78 +1,88 @@
-from chess.game.move import Move, CaptureMove
-from chess.game.board import Square
+import pytest
+from chess.game.move import SimpleMove, CaptureMove
+from chess.game.board import GameBoard, GameSquare
 from chess.enums.board import File, Rank
-from chess.enums.piece import PieceType
-
-
-class MockBoard:
-    def __init__(self) -> None:
-        self._removed_pieces = []
-        self._placed_pieces = []
-
-    def remove_piece(self, square, piece):
-        self._removed_pieces.append((square, piece))
-
-    def place_piece(self, square, piece):
-        self._placed_pieces.append((square, piece))
+from chess.enums.piece import PieceType, PieceColour
 
 
 class TestMove:
-    def test_move_creation(self):
-        source = Square(File.E, Rank.TWO)
-        destination = Square(File.E, Rank.FOUR)
-        piece = PieceType.PAWN
-        move = Move(source, destination, piece)
-        assert move.source == source
-        assert move.destination == destination
-        assert move.piece == piece
+    # TODO: Refactor test class to cover SimpleMove and CaptureMove.
+    @pytest.fixture
+    def board(self):
+        return GameBoard()
 
-    def test_move_execute(self):
-        source = Square(File.E, Rank.TWO)
-        destination = Square(File.E, Rank.FOUR)
-        piece = PieceType.PAWN
-        move = Move(source, destination, piece)
-        board = MockBoard()
-        move.execute(board)
-        # TODO:: implement move.execute then assert post-condition
+    @pytest.fixture
+    def simple_move(self):
+        return SimpleMove(
+            GameSquare(File.E, Rank.TWO), GameSquare(File.E, Rank.FOUR), PieceType.PAWN
+        )
 
-    def test_move_undo(self):
-        source = Square(File.E, Rank.TWO)
-        destination = Square(File.E, Rank.FOUR)
-        piece = PieceType.PAWN
-        move = Move(source, destination, piece)
-        board = MockBoard()
-        move.undo(board)
-        # TODO: implement move.undo then assert post-condition
+    def test_move_creation(self, simple_move):
+        assert simple_move.source == GameSquare(File.E, Rank.TWO)
+        assert simple_move.destination == GameSquare(File.E, Rank.FOUR)
+        assert simple_move.piece == PieceType.PAWN
+
+    def test_move_execute(self, board, simple_move):
+        board.place_piece(simple_move.source, simple_move.piece, PieceColour.WHITE)
+        simple_move.execute(board)
+        assert board.get_piece(simple_move.source) is None
+        assert board.get_piece(simple_move.destination) == (
+            PieceType.PAWN,
+            PieceColour.WHITE,
+        )
+
+    def test_move_undo(self, board, simple_move):
+        board.place_piece(simple_move.destination, simple_move.piece, PieceColour.WHITE)
+        simple_move.undo(board)
+        assert board.get_piece(simple_move.destination) is None
+        assert board.get_piece(simple_move.source) == (
+            PieceType.PAWN,
+            PieceColour.WHITE,
+        )
 
 
 class TestCaptureMove:
-    def test_capture_move_creation(self):
-        source = Square(File.E, Rank.FOUR)
-        destination = Square(File.D, Rank.FIVE)
-        piece = PieceType.PAWN
-        captured_piece = PieceType.PAWN
-        move = CaptureMove(source, destination, piece, captured_piece)
-        assert move.source == source
-        assert move.destination == destination
-        assert move.piece == piece
-        assert move.captured_piece == captured_piece
+    @pytest.fixture
+    def board(self):
+        return GameBoard()
 
-    def test_capture_move_execute(self):
-        source = Square(File.E, Rank.FOUR)
-        destination = Square(File.D, Rank.FIVE)
-        piece = PieceType.PAWN
-        captured_piece = PieceType.PAWN
-        move = CaptureMove(source, destination, piece, captured_piece)
-        board = MockBoard()
-        move.execute(board)
-        assert board._removed_pieces == [(destination, captured_piece)]
+    @pytest.fixture
+    def capture_move(self):
+        return CaptureMove(
+            GameSquare(File.E, Rank.FOUR),
+            GameSquare(File.D, Rank.FIVE),
+            PieceType.PAWN,
+            PieceType.PAWN,
+        )
 
-    def test_capture_move_undo(self):
-        source = Square(File.E, Rank.FOUR)
-        destination = Square(File.D, Rank.FIVE)
-        piece = PieceType.PAWN
-        captured_piece = PieceType.PAWN
-        move = CaptureMove(source, destination, piece, captured_piece)
-        board = MockBoard()
-        move.undo(board)
-        assert board._placed_pieces == [(destination, captured_piece)]
+    def test_capture_move_creation(self, capture_move):
+        assert capture_move.source == GameSquare(File.E, Rank.FOUR)
+        assert capture_move.destination == GameSquare(File.D, Rank.FIVE)
+        assert capture_move.piece == PieceType.PAWN
+        assert capture_move.get_captured_piece() == PieceType.PAWN
+
+    def test_capture_move_execute(self, board, capture_move):
+        board.place_piece(capture_move.source, capture_move.piece, PieceColour.WHITE)
+        board.place_piece(
+            capture_move.destination, capture_move.captured_piece, PieceColour.BLACK
+        )
+        capture_move.execute(board)
+        assert board.get_piece(capture_move.source) is None
+        assert board.get_piece(capture_move.destination) == (
+            PieceType.PAWN,
+            PieceColour.WHITE,
+        )
+
+    def test_capture_move_undo(self, board, capture_move):
+        board.place_piece(
+            capture_move.destination, capture_move.piece, PieceColour.WHITE
+        )
+        capture_move.undo(board)
+        assert board.get_piece(capture_move.destination) == (
+            capture_move.get_captured_piece(),
+            PieceColour.BLACK,
+        )
+        assert board.get_piece(capture_move.source) == (
+            capture_move.piece,
+            PieceColour.WHITE,
+        )
