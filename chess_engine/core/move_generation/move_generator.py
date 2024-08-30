7cdeb5071df04ed50e738fcc1ac336e83d1db054
samisnotinsane <sameen11@hotmail.com>
2024-08-30 14:21:20 +0100

    Adding pawn move generation from starting rank

--- chess_engine/core/move_generation/move_generator.py
+++ chess_engine/core/move_generation/move_generator.py
@@ -2,6 +2,9 @@ from chess_engine.core.enums import PieceType
 from chess_engine.core.interfaces.move_generator_interface import IMoveGenerator
 from chess_engine.core.models.move import Move
 from chess_engine.board.board_state import BoardState
+from chess_engine.core.move_generation.move_validators.base_validator import (
+    BaseValidator,
+)
 from chess_engine.core.move_generation.move_validators.pawn_validator import (
     PawnMoveValidator,
 )
@@ -17,7 +20,7 @@ class MoveGenerator(IMoveGenerator):
 
     def __init__(self) -> None:
         """Initialise the MoveGenerator with piece-specific move validators."""
-        self._validators = {
+        self._validators: dict[PieceType, BaseValidator] = {
             PieceType.PAWN: PawnMoveValidator(),
             # TODO: Initialise other piece validators.
         }
@@ -32,9 +35,17 @@ class MoveGenerator(IMoveGenerator):
         Returns:
             list[Move]: A list of all legal moves in the current position.
         """
-        pass
+        all_moves = []
+        for square, piece in board.get_all_pieces().items():
+            validator = self._validators.get(piece.piece_type)
+            if validator is None:
+                raise ValueError(
+                    f"No validator defined for piece {piece} in square {square}"
+                )
+            all_moves.extend(validator.generate_moves(board, square))
+        return [move for move in all_moves if self.is_move_legal(board, move)]
 
-    def is_move_legal(self, board: BoardState, move: Move):
+    def is_move_legal(self, board: BoardState, move: Move) -> bool:
         """
         Check if a given move is legal in the current position.
 
@@ -48,4 +59,23 @@ class MoveGenerator(IMoveGenerator):
         Returns:
             bool: True if the move is legal, False otherwise.
         """
-        pass
+        if not board._is_valid_square(move.from_square) or not board._is_valid_square(
+            move.to_square
+        ):
+            return False
+
+        piece = board.get_piece_at(move.from_square)
+        if piece is None:
+            return False
+
+        temp_board = board.copy()
+        temp_board.make_move(move)
+
+        if temp_board.is_king_in_check(piece.colour):
+            return False
+
+        # TODO: Add additional checks for:
+        # - castling through check
+        # - en passant
+
+        return True
--- chess_engine/core/move_generation/move_validators/pawn_validator.py
+++ chess_engine/core/move_generation/move_validators/pawn_validator.py
@@ -1,3 +1,4 @@
+from chess_engine.core.enums import Colour, MoveDirection
 from .base_validator import BaseValidator
 from chess_engine.core.models.move import Move
 from chess_engine.board.board_state import BoardState
@@ -25,4 +26,72 @@ class PawnMoveValidator(BaseValidator):
         Returns:
             list[Move]: A list of all possible moves for the pawn.
         """
-        pass
+        moves = []
+        pawn = board.get_piece_at(square)
+        if pawn:
+            direction = self._get_pawn_direction(pawn.colour)
+
+            if self._is_on_starting_rank(square, pawn.colour):
+                moves.extend(self._generate_forward_moves(board, square, direction))
+
+        return moves
+
+    def _get_pawn_direction(self, colour: Colour) -> MoveDirection:
+        """
+        Determine the direction in which the pawn moves based on its colour.
+
+        Args:
+            colour (Colour): The colour of the pawn.
+
+        Returns:
+            MoveDirection: The direction of movement, either:
+                MoveDirection.UP for Colour.WHITE or
+                MoveDirection.DOWN for Colour.BLACK
+        """
+        return MoveDirection.UP if colour == Colour.WHITE else MoveDirection.DOWN
+
+    def _is_on_starting_rank(self, square: Square, colour: Colour) -> bool:
+        """
+        Check if the pawn is on its starting rank.
+
+        Args:
+            square (Square): The square containing the pawn.
+            colour (Colour): The colour of the pawn.
+
+        Returns: True if the pawn is on its starting rank, False otherwise.
+        """
+        return (colour == Colour.WHITE and square.rank == 1) or (
+            colour == Colour.BLACK and square.rank == 6
+        )
+
+    def _generate_forward_moves(
+        self, board: BoardState, square: Square, direction: MoveDirection
+    ) -> list[Move]:
+        """
+        Generate forward moves for a pawn from its starting position.
+
+        Args:
+            board (BoardState): The current board state.
+            square (Square): The square containing the pawn.
+            direction (MoveDirection): The direction of the pawn moves.
+
+        Returns:
+            list[Move]: A list of possible forward moves.
+        """
+        moves = []
+        move_direction = 0
+        if direction == MoveDirection.UP:
+            move_direction = 1
+        if direction == MoveDirection.DOWN:
+            move_direction = -1
+
+        forward = Square(square.file, square.rank + move_direction)
+
+        if not board.is_square_occupied(forward):
+            moves.append(Move(square, forward))
+
+            double_forward = Square(square.file, square.rank + (2 * move_direction))
+            if not board.is_square_occupied(double_forward):
+                moves.append(Move(square, double_forward))
+
+        return moves
